b17566c6c62f1c70c6619a8535714269d078e183
D3N15T0R <for_letters_736@mail.ru>
2019-02-15 23:48:46 +0300

    ContractAnnotation

---
+++ JetBrainsAnnotationsExamples/ContractAnnotation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace ContractAnnotation
+{
+	internal class Program
+	{
+		private static void Main()
+		{
+			var nullString = GenerateString();
+
+			if (StringIsNotNull(nullString))
+			{
+				Console.WriteLine(nullString.Length);
+			}
+
+			//ThrowExceptionWhenNull(nullString);
+			//var length = nullString.Length;
+			//Console.WriteLine(length);
+
+			Console.WriteLine("End");
+			Console.ReadLine();
+		}
+
+		[CanBeNull]
+		private static string GenerateString() => new Random().Next() % 2 == 0 ? string.Empty : null;
+
+		//[ContractAnnotation("null => false")]
+		private static bool StringIsNotNull([CanBeNull] string parameter) => !(parameter is null);
+
+		//[ContractAnnotation("null => stop")]
+		private static void ThrowExceptionWhenNull([CanBeNull] string parameter)
+		{
+			if (parameter is null)
+			{
+				throw new ArgumentNullException(nameof(parameter));
+			}
+		}
+	}
+}
