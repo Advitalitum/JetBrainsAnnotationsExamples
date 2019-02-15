57df4eb45d588234567a7f5af53fc422fcffc14b
D3N15T0R <for_letters_736@mail.ru>
2019-02-15 23:32:42 +0300

    Start

---
+++ JetBrainsAnnotationsExamples/CanBeNull/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace CanBeNull
+{
+	internal class Program
+	{
+		private const string NullString = default(string);
+
+		private static void Main()
+		{
+			var length = DangerouslyGetLength(NullString);
+
+			Console.WriteLine(length);
+			Console.WriteLine("End");
+			Console.ReadLine();
+		}
+
+		private static int DangerouslyGetLength([NotNull] string parameter) => parameter.Length;
+	}
+}
---
+++ JetBrainsAnnotationsExamples/FodyNullGuard/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using JetBrains.Annotations;
+using NullGuard;
+
+namespace FodyNullGuard
+{
+	[NullGuard(ValidationFlags.All)]
+	internal class Program
+	{
+		private const string NullString = default(string);
+
+		private static void Main()
+		{
+			var length = DangerouslyGetLength(NullString);
+
+			Console.WriteLine(length);
+			Console.WriteLine("End");
+			Console.ReadLine();
+		}
+
+		private static int DangerouslyGetLength([CanBeNull] string parameter) => parameter?.Length ?? int.MinValue;
+	}
+}
