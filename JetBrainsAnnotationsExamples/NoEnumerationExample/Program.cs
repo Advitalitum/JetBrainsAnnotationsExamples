f86faa80a14e84fa397d2dfbfe944115ce638c87
D3N15T0R <for_letters_736@mail.ru>
2019-02-16 17:53:48 +0300

    NoEnumerationExample

---
+++ JetBrainsAnnotationsExamples/NoEnumerationExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NoEnumerationExample
+{
+	internal class Program
+	{
+		private static void Main()
+		{
+			var enumerable = GetEnumerable();
+
+			CheckNotNull(enumerable);
+
+			var list = enumerable.ToList();
+			list.ForEach(Console.WriteLine);
+
+			Console.WriteLine("End");
+			Console.ReadLine();
+		}
+
+		private static IEnumerable<string> GetEnumerable() => Enumerable.Repeat(Guid.NewGuid().ToString(), 10);
+
+		private static void CheckNotNull(IEnumerable<string> enumerable)
+		{
+			if (enumerable is null)
+				throw new ArgumentNullException(nameof(enumerable));
+		}
+	}
+}
