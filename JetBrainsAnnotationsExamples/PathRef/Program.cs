99ef418fa8ecdf6f857225f5d6f5d82af28c9af1
D3N15T0R <for_letters_736@mail.ru>
2019-02-16 17:31:38 +0300

    PathRef, RegexPattern

---
+++ JetBrainsAnnotationsExamples/PathRef/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using JetBrains.Annotations;
+
+namespace PathRef
+{
+	internal class Program
+	{
+		private static void Main()
+		{
+			var fileContent = ReadFileContent("SomeFile.file");
+
+			Console.WriteLine(fileContent);
+			Console.WriteLine("End");
+			Console.ReadLine();
+		}
+
+		private static string ReadFileContent([PathReference] string path)
+			=> File.ReadAllText(path);
+	}
+}
---
+++ JetBrainsAnnotationsExamples/RegexExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using JetBrains.Annotations;
+
+namespace RegexExample
+{
+	internal class Program
+	{
+		private static void Main()
+		{
+			var regex = CreateRegexWithPattern("[a-z]");
+
+			Console.WriteLine(regex.IsMatch("123"));
+			Console.WriteLine("End");
+			Console.ReadLine();
+		}
+
+		private static Regex CreateRegexWithPattern([RegexPattern] string pattern) =>
+			new Regex(pattern);
+	}
+}
